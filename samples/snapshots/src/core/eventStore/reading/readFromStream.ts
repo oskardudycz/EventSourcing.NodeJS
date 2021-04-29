e6a91b9b5ef41cd69ef2a77b73d0e45ed83686bb
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-04-29 16:07:55 +0200

    Added the snapshot from stream function

--- samples/snapshots/src/core/eventStore/reading/readFromStream.ts
+++ samples/snapshots/src/core/eventStore/reading/readFromStream.ts
@@ -3,17 +3,19 @@ import { ReadStreamOptions } from '@eventstore/db-client/dist/streams';
 
 import { Event } from '../../events';
 
+export type STREAM_NOT_FOUND = 'STREAM_NOT_FOUND';
+
 export async function readFromStream<StreamEvent extends Event>(
   client: EventStoreDBClient,
   streamName: string,
   options?: ReadStreamOptions
-): Promise<StreamEvent[] | undefined> {
+): Promise<StreamEvent[] | STREAM_NOT_FOUND> {
   let events;
   try {
     events = await client.readStream(streamName, options);
   } catch (error) {
     if (error.type == ErrorType.STREAM_NOT_FOUND) {
-      return undefined;
+      return 'STREAM_NOT_FOUND';
     }
 
     throw error;
