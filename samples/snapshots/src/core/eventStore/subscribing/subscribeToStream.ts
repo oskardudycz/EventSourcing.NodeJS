30bb685efc2cdfe2e28efa7e27b380135232862d
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-06-15 13:26:08 +0200

    Removed redundant invoices module

--- samples/snapshots/src/core/eventStore/subscribing/subscribeToStream.ts
+++ samples/snapshots/src/core/eventStore/subscribing/subscribeToStream.ts
@@ -1,31 +1,56 @@
-import { EventStoreDBClient, StreamSubscription } from '@eventstore/db-client';
+import {
+  EventStoreDBClient,
+  Position,
+  StreamSubscription,
+} from '@eventstore/db-client';
 import { SubscribeToStreamOptions } from '@eventstore/db-client/dist/streams';
 
 import { Event } from '../../events';
+import { pipeResultAsync } from '../../primitives/pipe';
+import { Result, success } from '../../primitives/result';
+import { FAILED_TO_STORE_CHECKPOINT } from './checkpoints';
 
 export async function subscribeToStream<StreamEvent extends Event>(
+  loadCheckpoint: (
+    subscriptionId: string
+  ) => Promise<Result<Position | undefined>>,
+  storeCheckpoint: (
+    subscriptionId: string,
+    revision: bigint
+  ) => Promise<Result<true, FAILED_TO_STORE_CHECKPOINT>>,
   client: EventStoreDBClient,
   streamName: string,
-  handleEvent: (event: StreamEvent, position: bigint) => Promise<void>,
+  subscriptionId: string,
+  handleEvent: (
+    event: StreamEvent,
+    options: { revision: bigint; streamName: string }
+  ) => Promise<void>,
   options?: SubscribeToStreamOptions
-): Promise<StreamSubscription> {
-  return client
-    .subscribeToStream(streamName)
-    .on('data', function (resolvedEvent) {
-      if (!resolvedEvent.event) {
-        console.log(`Event without data received`);
-        return;
-      }
-      if (!resolvedEvent.commitPosition) {
-        console.log(`Event without data received`);
-        return;
-      }
+): Promise<Result<StreamSubscription>> {
+  return pipeResultAsync(loadCheckpoint, async (currentPosition) => {
+    return success(
+      client
+        .subscribeToStream(streamName, options)
+        .on('data', async function (resolvedEvent) {
+          if (!resolvedEvent.event) {
+            console.log(`Event without data received`);
+            return;
+          }
 
-      const event = {
-        type: resolvedEvent.event.type,
-        data: resolvedEvent.event.data,
-      } as StreamEvent;
+          const event = {
+            type: resolvedEvent.event.type,
+            data: resolvedEvent.event.data,
+          } as StreamEvent;
 
-      handleEvent(event, resolvedEvent.commitPosition);
-    });
+          //TODO: add here some retry logic
+          await handleEvent(event, {
+            revision: resolvedEvent.event.revision,
+            streamName,
+          });
+
+          //TODO: add here some retry logic
+          await storeCheckpoint(subscriptionId, resolvedEvent.event.revision);
+        })
+    );
+  })(subscriptionId);
 }
