f7ca80d8566e48ea97431cc3f26e595d1c402a22
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-07-19 10:14:32 +0200

    Added subscription reconnection code

--- samples/snapshots/src/subscription.ts
+++ samples/snapshots/src/subscription.ts
@@ -5,48 +5,86 @@ import {
 } from '#core/eventStore/subscribing';
 import { getEventStore } from '#core/eventStore';
 import { storeCashRegisterSnapshotOnSubscription } from './cashiers/processCashRegister/storeCashRegisterSnapshotOnSubscription';
+import { sleep } from '#core/primitives';
 
-async function subscribe(reject: (error: any) => void, resolve: () => void) {
-  try {
-    const eventStore = getEventStore();
+async function subscribe() {
+  return new Promise<void>(async (resolve, reject) => {
+    try {
+      const eventStore = getEventStore();
 
-    const subscriptionResult = await subscribeToAll(
-      eventStore,
-      (subscriptionId) => loadCheckpoint(eventStore, subscriptionId),
-      (subscriptionId, position) =>
-        storeCheckpoint(eventStore, subscriptionId, position),
-      [
-        (event, options) =>
-          storeCashRegisterSnapshotOnSubscription(event, options),
-      ],
-      'cash_register_subscription'
-    );
+      const subscriptionResult = await subscribeToAll(
+        eventStore,
+        (subscriptionId) => loadCheckpoint(eventStore, subscriptionId),
+        (subscriptionId, position) =>
+          storeCheckpoint(eventStore, subscriptionId, position),
+        [
+          (event, options) =>
+            storeCashRegisterSnapshotOnSubscription(event, options),
+        ],
+        'cash_register_subscription'
+      );
 
-    if (subscriptionResult.isError === true) {
-      reject(subscriptionResult.error ?? 'ERROR WHILE SUBSCRIBING');
-      return;
+      if (subscriptionResult.isError === true) {
+        throw subscriptionResult.error ?? 'ERROR WHILE SUBSCRIBING';
+      }
+
+      const subscription = subscriptionResult.value;
+
+      subscription
+        .on('error', async (error) => {
+          console.error(
+            `Received error: ${error ?? 'UNEXPECTED ERROR'}.
+          Reconnecting.`
+          );
+          reject(error);
+        })
+        .on('close', async () => {
+          console.error(
+            `Subscription closed.
+          Reconnecting.`
+          );
+          reject();
+        })
+        .on('end', () => {
+          console.info(
+            `Received 'end' event.
+          Stopping subscription.`
+          );
+          resolve();
+        });
+    } catch (error) {
+      console.error(
+        `Received error while subscribing: ${error ?? 'UNEXPECTED ERROR'}.
+      Reconnecting.`
+      );
+      reject(error ?? 'UNEXPECTED ERROR');
     }
+  });
+}
 
-    const subscription = subscriptionResult.value;
+async function reconnect(): Promise<void> {
+  do {
+    try {
+      console.info('Starting reconnection');
+      await sleep(1000);
 
-    subscription
-      .on('error', (error) => {
-        console.error(error ?? 'UNEXPECTED ERROR CLOSING');
-        reject(error);
-      })
-      .on('close', () => {
-        resolve();
-      })
-      .on('end', () => {
-        resolve();
-      });
-  } catch (error) {
-    reject(error);
-  }
+      await subscribe();
+    } catch (error) {
+      console.error(
+        `Received error while reconnecting: ${error ?? 'UNEXPECTED ERROR'}.
+        Reconnecting.`
+      );
+    }
+  } while (true);
 }
 
 (async () => {
   return new Promise<void>(async (resolve, reject) => {
-    subscribe(resolve, reject);
+    try {
+      await subscribe();
+      resolve();
+    } catch (error) {
+      await reconnect();
+    }
   });
 })();
