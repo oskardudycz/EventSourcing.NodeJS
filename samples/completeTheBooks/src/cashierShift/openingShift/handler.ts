78ba5579a4086b3df8656192bac2e73b14a8ae46
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-08-10 13:31:30 +0200

    Added NotInitiatedCashierShift type for initiating the new stream when cash register was placed at the workstation

--- samples/completeTheBooks/src/cashierShift/openingShift/handler.ts
+++ samples/completeTheBooks/src/cashierShift/openingShift/handler.ts
@@ -5,6 +5,7 @@ import {
   CashierShiftEvent,
   CashierShiftStatus,
   getCashierShiftFrom,
+  isCashierShift,
   SHIFT_ALREADY_OPENED,
 } from '../cashierShift';
 
@@ -34,15 +35,22 @@ export function handleOpenShift(
 ): Result<ShiftOpened, SHIFT_ALREADY_OPENED> {
   const cashierShift = getCashierShiftFrom(events);
 
-  if (cashierShift.status === CashierShiftStatus.Opened) {
+  if (
+    isCashierShift(cashierShift) &&
+    cashierShift.status !== CashierShiftStatus.Opened
+  ) {
     return failure('SHIFT_ALREADY_OPENED');
   }
+
+  const currentShiftNumber = isCashierShift(cashierShift)
+    ? cashierShift.number
+    : 0;
 
   return success({
     type: 'shift-opened',
     data: {
-      shiftNumber: cashierShift.number,
-      cashRegisterId: command.data.cashRegisterId,
+      shiftNumber: currentShiftNumber,
+      cashRegisterId: cashierShift.cashRegisterId,
       cashierId: command.data.cashierId,
       declaredStartAmount: command.data.declaredStartAmount,
       startedAt: getCurrentTime(),
