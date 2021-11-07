0726a4de7d8a7e10f83e9f894a855bd8f47d3e2b
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-11-07 11:34:53 +0100

    Added e2e test and fixed bugs in handling

--- samples/optimisticConcurrency/src/shoppingCarts/index.ts
+++ samples/optimisticConcurrency/src/shoppingCarts/index.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express';
-import { route as routeConfirmShoppingCart } from './opening';
+import { route as routeOpenShoppingCart } from './opening';
+import { route as routeAddProductItem } from './addingProductItem';
+import { route as routeRemoveProductItem } from './removingProductItem';
+import { route as routeConfirmShoppingCart } from './confirming';
+import { route as routeGetShoppingCartById } from './gettingById';
 
 export const shoppingCartRouter = Router();
 
+routeOpenShoppingCart(shoppingCartRouter);
+routeAddProductItem(shoppingCartRouter);
+routeRemoveProductItem(shoppingCartRouter);
 routeConfirmShoppingCart(shoppingCartRouter);
+routeGetShoppingCartById(shoppingCartRouter);
 export * from './shoppingCart';
