021bf1f92eb7f446829232e57e50e4266cbe6160
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-02-17 13:49:57 +0100

    Made event types immutable

--- samples/webinar/src/index.ts
+++ samples/webinar/src/index.ts
@@ -2,31 +2,31 @@
 /// Events
 //////////////////////////////////////
 
-export type ShoppingCartOpened = {
+export type ShoppingCartOpened = Readonly<{
   shoppingCartId: string;
   clientId: string;
   openedAt: Date;
-};
+}>;
 
-export type ProductItemAddedToShoppingCart = {
+export type ProductItemAddedToShoppingCart = Readonly<{
   shoppingCartId: string;
   productItem: ProductItem;
-};
+}>;
 
-export type ProductItem = {
+export type ProductItem = Readonly<{
   productId: string;
   quantity: number;
-};
+}>;
 
-export type ProductItemRemovedFromShoppingCart = {
+export type ProductItemRemovedFromShoppingCart = Readonly<{
   shoppingCartId: string;
   productItem: ProductItem;
-};
+}>;
 
-export type ShoppingCartConfirmed = {
+export type ShoppingCartConfirmed = Readonly<{
   shoppingCartId: string;
   confirmedAt: Date;
-};
+}>;
 
 export type ShoppingCartEvent =
   | ShoppingCartOpened
