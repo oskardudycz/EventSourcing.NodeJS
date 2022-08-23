b9c93d7db75d106235220f6c554ba9122eb50137
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-08-23 15:48:24 +0200

    Fixed failing CRUD e2e test

--- samples/from_crud_to_eventsourcing/src/crud/db/index.ts
+++ samples/from_crud_to_eventsourcing/src/crud/db/index.ts
@@ -1,13 +1,10 @@
-import createConnectionPool, { sql } from '@databases/pg';
+import { sql } from '@databases/pg';
 import tables from '@databases/pg-typed';
 import DatabaseSchema from './__generated__';
 import databaseSchema from './__generated__/schema.json';
 
 export { sql };
 export { carts, cartItems };
-
-const db = createConnectionPool();
-export default db;
 
 const { cart: carts, cart_item: cartItems } = tables<DatabaseSchema>({
   databaseSchema,
