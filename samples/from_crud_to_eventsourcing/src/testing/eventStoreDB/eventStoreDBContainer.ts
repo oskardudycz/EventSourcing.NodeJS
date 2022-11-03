18d6eae2b3013902ac17d6a677ff57df93255947
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-11-03 16:55:20 +0100

    Upgraded packages, ESDB version and fixed linting

--- samples/from_crud_to_eventsourcing/src/testing/eventStoreDB/eventStoreDBContainer.ts
+++ samples/from_crud_to_eventsourcing/src/testing/eventStoreDB/eventStoreDBContainer.ts
@@ -1,12 +1,13 @@
 import { EventStoreDBClient } from '@eventstore/db-client';
 import { GenericContainer, StartedTestContainer } from 'testcontainers';
+import { Environment } from 'testcontainers/dist/docker/types';
 import { AbstractStartedContainer } from 'testcontainers/dist/modules/abstract-started-container';
 
 const EVENTSTOREDB_PORT = 2113;
 const EVENTSTOREDB_TCP_PORT = 1113;
 const EVENTSTOREDB_TCP_PORTS = [EVENTSTOREDB_TCP_PORT, EVENTSTOREDB_PORT];
 const EVENTSTOREDB_IMAGE_NAME = 'eventstore/eventstore';
-const EVENTSTOREDB_IMAGE_TAG = '21.10.2-buster-slim';
+const EVENTSTOREDB_IMAGE_TAG = '21.10.8-buster-slim';
 
 export class EventStoreDBContainer extends GenericContainer {
   private readonly tcpPorts = EVENTSTOREDB_TCP_PORTS;
@@ -19,28 +20,32 @@ export class EventStoreDBContainer extends GenericContainer {
   ) {
     super(image);
 
-    if (runProjections) {
-      this.withEnv('EVENTSTORE_RUN_PROJECTIONS', 'ALL');
-    }
+    const environment: Environment = {
+      ...(runProjections
+        ? {
+            EVENTSTORE_RUN_PROJECTIONS: 'ALL',
+          }
+        : {}),
+      ...(isInsecure
+        ? {
+            EVENTSTORE_INSECURE: 'true',
+          }
+        : {}),
+      ...(!emptyDatabase
+        ? {
+            EVENTSTORE_MEM_DB: 'false',
+            EVENTSTORE_DB: '/data/integration-tests',
+          }
+        : {}),
+      EVENTSTORE_CLUSTER_SIZE: '1',
+      EVENTSTORE_START_STANDARD_PROJECTIONS: 'true',
+      EVENTSTORE_EXT_TCP_PORT: `${EVENTSTOREDB_TCP_PORT}`,
+      EVENTSTORE_HTTP_PORT: `${EVENTSTOREDB_PORT}`,
+      EVENTSTORE_ENABLE_EXTERNAL_TCP: 'true',
+      EVENTSTORE_ENABLE_ATOM_PUB_OVER_HTTP: 'true',
+    };
 
-    if (isInsecure) {
-      this.withEnv('EVENTSTORE_INSECURE', 'true');
-    }
-
-    if (!emptyDatabase) {
-      this.withEnv('EVENTSTORE_MEM_DB', 'false').withEnv(
-        'EVENTSTORE_DB',
-        '/data/integration-tests'
-      );
-    }
-
-    this.withEnv('EVENTSTORE_CLUSTER_SIZE', '1')
-      .withEnv('EVENTSTORE_START_STANDARD_PROJECTIONS', 'true')
-      .withEnv('EVENTSTORE_EXT_TCP_PORT', `${EVENTSTOREDB_TCP_PORT}`)
-      .withEnv('EVENTSTORE_HTTP_PORT', `${EVENTSTOREDB_PORT}`)
-      .withEnv('EVENTSTORE_ENABLE_EXTERNAL_TCP', 'true')
-      .withEnv('EVENTSTORE_ENABLE_ATOM_PUB_OVER_HTTP', 'true')
-      .withExposedPorts(...this.tcpPorts);
+    this.withEnvironment(environment).withExposedPorts(...this.tcpPorts);
   }
 
   async start(): Promise<StartedEventStoreDBContainer> {
