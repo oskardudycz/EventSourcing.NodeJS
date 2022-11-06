cc6151970e173775212370bc71259b3c1599d541
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-11-06 14:53:12 +0100

    Got rid of mappers, shoppingCartRepository and query handlers

--- samples/unwrapping_onion/src/unpeeled/ecommerce/shoppingCarts/controllers/index.ts
+++ samples/unwrapping_onion/src/unpeeled/ecommerce/shoppingCarts/controllers/index.ts
@@ -1,11 +1,7 @@
 import { EventBusFactory } from '#core/events';
-import { QueryBusFactory } from '#core/queries';
 import { MongoClient } from 'mongodb';
 import { ShoppingCartController } from './shoppingCartController';
 
 export const getControllers = (mongo: MongoClient) => {
-  return [
-    new ShoppingCartController(mongo, EventBusFactory(), QueryBusFactory())
-      .router,
-  ];
+  return [new ShoppingCartController(mongo, EventBusFactory()).router];
 };
