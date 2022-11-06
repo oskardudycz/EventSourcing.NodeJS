fc7fb613a84203c6fb3fcb0b0565c869c460060e
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-11-06 10:40:53 +0100

    Moved command handlers directly into controller

--- samples/unwrapping_onion/src/unpeeled/ecommerce/shoppingCarts/application/index.ts
+++ samples/unwrapping_onion/src/unpeeled/ecommerce/shoppingCarts/application/index.ts
@@ -6,13 +6,9 @@ import { GetShoppingCartById } from 'src/unpeeled/ecommerce/shoppingCarts/applic
 import { AddProductItemToShoppingCart } from 'src/unpeeled/ecommerce/shoppingCarts/application/commands/shoppingCarts/addProductItemToShoppingCart';
 import { RemoveProductItemFromShoppingCart } from './commands/shoppingCarts/removeProductItemFromShoppingCart';
 import { ShoppingCartMapper } from './mappers/shoppingCartMapper';
-import { OpenShoppingCartHandler } from './commandHandlers/openShoppingCartHandler';
-import { AddProductItemToShoppingCartHandler } from './commandHandlers/addProductToShoppingCartHandler';
-import { RemoveProductItemFromShoppingCartHandler } from './commandHandlers/removeProductFromShoppingCartHandler';
 import { GetShoppingCartByIdHandler } from './queryHandlers/getShoppingCartByIdQueryHandler';
 import { ShoppingCartRepository } from 'src/unpeeled/ecommerce/shoppingCarts/infrastructure/shoppingCartRepository';
 import { ConfirmShoppingCart } from './commands/shoppingCarts/confirmShoppingCart';
-import { ConfirmShoppingCartHandler } from './commandHandlers/confirmShoppingCart';
 import { GetCustomerShoppingHistory } from './queries/getCustomerShoppingHistory';
 import { GetCustomerShoppingHistoryHandler } from './queryHandlers/getCustomerShoppingHistoryHandler';
 import { CustomerShoppingHistoryMapper } from './mappers/customerShoppingHistoryMapper';
@@ -20,35 +16,8 @@ import { EventBusFactory } from '#core/events';
 
 const registerHandlers = (mongo: MongoClient) => {
   const repository = new ShoppingCartRepository(mongo);
-  const eventBus = EventBusFactory();
-
-  const shoppingCartMapper = new ShoppingCartMapper();
   const shoppingHistoryMapper = new CustomerShoppingHistoryMapper();
 
-  registerCommandHandler(
-    OpenShoppingCart,
-    new OpenShoppingCartHandler(repository, shoppingCartMapper, eventBus)
-  );
-  registerCommandHandler(
-    AddProductItemToShoppingCart,
-    new AddProductItemToShoppingCartHandler(
-      repository,
-      shoppingCartMapper,
-      eventBus
-    )
-  );
-  registerCommandHandler(
-    RemoveProductItemFromShoppingCart,
-    new RemoveProductItemFromShoppingCartHandler(
-      repository,
-      shoppingCartMapper,
-      eventBus
-    )
-  );
-  registerCommandHandler(
-    ConfirmShoppingCart,
-    new ConfirmShoppingCartHandler(repository, shoppingCartMapper, eventBus)
-  );
   registerQueryHandler(
     GetShoppingCartById,
     new GetShoppingCartByIdHandler(repository)
--- samples/unwrapping_onion/src/unpeeled/ecommerce/shoppingCarts/controllers/index.ts
+++ samples/unwrapping_onion/src/unpeeled/ecommerce/shoppingCarts/controllers/index.ts
@@ -1,9 +1,19 @@
-import { CommandBusFactory } from '#core/commands';
+import { EventBusFactory } from '#core/events';
 import { QueryBusFactory } from '#core/queries';
+import { MongoClient } from 'mongodb';
+import { ShoppingCartMapper } from '../application/mappers/shoppingCartMapper';
+import { ShoppingCartRepository } from '../infrastructure/shoppingCartRepository';
 import { ShoppingCartController } from './shoppingCartController';
 
-const controllers = [
-  new ShoppingCartController(CommandBusFactory(), QueryBusFactory()).router,
-];
-
-export default controllers;
+export const getControllers = (mongo: MongoClient) => {
+  const repository = new ShoppingCartRepository(mongo);
+  const shoppingCartMapper = new ShoppingCartMapper();
+  return [
+    new ShoppingCartController(
+      repository,
+      shoppingCartMapper,
+      EventBusFactory(),
+      QueryBusFactory()
+    ).router,
+  ];
+};
