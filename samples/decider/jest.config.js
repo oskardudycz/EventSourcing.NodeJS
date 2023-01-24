731d9781d314c088f53b4d29c7910e3eed52eece
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-01-24 10:56:56 +0100

    Updated decider sample config to be aligned with other samples.

---
+++ samples/decider/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '#core/(.*)': '<rootDir>/src/core/$1',
+    '#config': '<rootDir>/config.ts',
+    '#testing/(.*)': '<rootDir>/src/testing/$1',
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json',
+    },
+  },
+};
