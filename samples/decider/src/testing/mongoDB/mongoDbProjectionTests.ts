d7d788264534ddba2cd29ebf461dc6021adcafc6
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-02-01 16:22:27 +0100

    Refactored the testing framework for projections

--- samples/decider/src/testing/mongoDB/mongoDbProjectionTests.ts
+++ samples/decider/src/testing/mongoDB/mongoDbProjectionTests.ts
@@ -11,64 +11,92 @@ import { mongoObjectId, retryIfNotFound } from '#core/mongoDB';
 import { SubscriptionResolvedEvent } from '#core/subscriptions';
 import { AllStreamRecordedEvent } from '@eventstore/db-client';
 
-export const given = <
+type HasRevisionOrPosition = { revision: number } | { position: number | Long };
+type DocumentWithRevisionOrPosition = Document & HasRevisionOrPosition;
+
+type EventWithMetadata<E> = { event: E; revision?: bigint; position?: bigint };
+
+export type Spec<
   E extends Event,
-  Doc extends Document & HasRevisionOrPosition = Document &
-    HasRevisionOrPosition
->(
-  collection: Collection<Doc>,
-  ...events: (E | { event: E; revision?: bigint; position?: bigint })[]
-) => {
-  return {
-    when: (
-      project: (event: SubscriptionResolvedEvent) => Promise<UpdateResult>
-    ) => {
-      return {
-        then: async (
-          id: string,
-          expected: Doc,
-          options?: { changed?: number; acknowledged?: number }
-        ) => {
-          let position = 0n;
-          let changesCount = 0;
-          let acknowledgementCount = 0;
-
-          for (const event of events) {
-            const options = {
-              position:
-                'position' in event ? event.position ?? position : position,
-              revision:
-                'revision' in event ? event.revision ?? position : position,
-            };
-
-            const projectedEvent = toSubscriptionEvent(
-              'event' in event ? event.event : event,
-              options
-            );
-
-            const result = await project(projectedEvent);
-
-            changesCount += result.upsertedCount + result.modifiedCount;
-            acknowledgementCount += result.acknowledged ? 1 : 0;
-
-            position++;
-          }
-
-          expect(changesCount).toBe(options?.changed ?? events.length);
-          expect(acknowledgementCount).toBe(
-            options?.acknowledged ?? events.length
-          );
-
-          await assertUpdated(collection, id, expected);
-        },
-      };
-    },
+  Doc extends DocumentWithRevisionOrPosition = DocumentWithRevisionOrPosition
+> = (...givenEvents: (E | EventWithMetadata<E>)[]) => {
+  when: (...events: (E | EventWithMetadata<E>)[]) => {
+    then: (id: string, expected: Doc) => Promise<void>;
+    thenUpdated: (times: number) => Promise<void>;
+    thenNotUpdated: () => Promise<void>;
   };
 };
 
-type HasRevisionOrPosition = { revision: number } | { position: number | Long };
+export const Spec = {
+  for: <
+    E extends Event,
+    Doc extends DocumentWithRevisionOrPosition = DocumentWithRevisionOrPosition
+  >(
+    collection: Collection<Doc>,
+    project: (event: SubscriptionResolvedEvent) => Promise<UpdateResult>
+  ): Spec<E, Doc> => {
+    {
+      return (...givenEvents: (E | EventWithMetadata<E>)[]) => {
+        return {
+          when: (...events: (E | EventWithMetadata<E>)[]) => {
+            const allEvents = [...givenEvents, ...events];
 
-const assertUpdated = async <Doc extends Document & HasRevisionOrPosition>(
+            const run = async () => {
+              let position = 0n;
+              let changesCount = 0;
+              let acknowledgementCount = 0;
+
+              for (const event of allEvents) {
+                const options = {
+                  position:
+                    'position' in event ? event.position ?? position : position,
+                  revision:
+                    'revision' in event ? event.revision ?? position : position,
+                };
+
+                const projectedEvent = toSubscriptionEvent(
+                  'event' in event ? event.event : event,
+                  options
+                );
+
+                const result = await project(projectedEvent);
+
+                changesCount += result.upsertedCount + result.modifiedCount;
+                acknowledgementCount += result.acknowledged ? 1 : 0;
+
+                position++;
+              }
+
+              expect(acknowledgementCount).toBe(allEvents.length);
+
+              return { changesCount };
+            };
+
+            const thenUpdated = async (times: number): Promise<void> => {
+              const { changesCount } = await run();
+
+              expect(changesCount - givenEvents.length).toBe(times);
+            };
+
+            return {
+              thenUpdated,
+              thenNotUpdated: () => {
+                return thenUpdated(0);
+              },
+              then: async (id: string, expected: Doc) => {
+                await thenUpdated(events.length);
+
+                return assertUpdated(collection, id, expected);
+              },
+            };
+          },
+        };
+      };
+    }
+  },
+};
+
+const assertUpdated = async <Doc extends DocumentWithRevisionOrPosition>(
   collection: Collection<Doc>,
   id: string,
   expected: Doc
