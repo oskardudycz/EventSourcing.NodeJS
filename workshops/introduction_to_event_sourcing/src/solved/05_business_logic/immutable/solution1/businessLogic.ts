29c2ab6a30e045eaf18bb21d5c8bff7e74d1d845
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-02-19 19:41:19 +0100

    Added solution for business logic immutable exercise

--- workshops/introduction_to_event_sourcing/src/solved/05_business_logic/immutable/solution1/businessLogic.ts
+++ workshops/introduction_to_event_sourcing/src/solved/05_business_logic/immutable/solution1/businessLogic.ts
@@ -4,50 +4,49 @@
 
 import {
   EventStore,
-  getShoppingCart,
+  evolve,
   PricedProductItem,
+  ProductItemAddedToShoppingCart,
+  ProductItemRemovedFromShoppingCart,
   ShoppingCart,
-  ShoppingCartEvent,
+  ShoppingCartCanceled,
+  ShoppingCartConfirmed,
+  ShoppingCartOpened,
   ShoppingCartStatus,
 } from './businessLogic.solved.test';
 
+export type OpenShoppingCart = {
+  shoppingCartId: string;
+  clientId: string;
+  now: Date;
+};
+
+export type AddProductItemToShoppingCart = {
+  shoppingCartId: string;
+  productItem: PricedProductItem;
+};
+
+export type RemoveProductItemFromShoppingCart = {
+  shoppingCartId: string;
+  productItem: PricedProductItem;
+};
+
+export type ConfirmShoppingCart = {
+  shoppingCartId: string;
+  now: Date;
+};
+
+export type CancelShoppingCart = {
+  shoppingCartId: string;
+  now: Date;
+};
+
 export type ShoppingCartCommand =
-  | {
-      type: 'OpenShoppingCart';
-      data: {
-        shoppingCartId: string;
-        clientId: string;
-        now: Date;
-      };
-    }
-  | {
-      type: 'AddProductItemToShoppingCart';
-      data: {
-        shoppingCartId: string;
-        productItem: PricedProductItem;
-      };
-    }
-  | {
-      type: 'RemoveProductItemFromShoppingCart';
-      data: {
-        shoppingCartId: string;
-        productItem: PricedProductItem;
-      };
-    }
-  | {
-      type: 'ConfirmShoppingCart';
-      data: {
-        shoppingCartId: string;
-        now: Date;
-      };
-    }
-  | {
-      type: 'CancelShoppingCart';
-      data: {
-        shoppingCartId: string;
-        now: Date;
-      };
-    };
+  | OpenShoppingCart
+  | AddProductItemToShoppingCart
+  | RemoveProductItemFromShoppingCart
+  | ConfirmShoppingCart
+  | CancelShoppingCart;
 
 //////////////////////////////////////
 /// Decide
@@ -76,99 +75,115 @@ export const assertProductItemExists = (
   }
 };
 
-const decide = (
-  { type, data: command }: ShoppingCartCommand,
+export const openShoppingCart = (
+  command: OpenShoppingCart
+): ShoppingCartOpened => {
+  return {
+    type: 'ShoppingCartOpened',
+    data: {
+      shoppingCartId: command.shoppingCartId,
+      clientId: command.clientId,
+      openedAt: command.now,
+    },
+  };
+};
+
+export const addProductItemToShoppingCart = (
+  command: AddProductItemToShoppingCart,
   shoppingCart: ShoppingCart
-): ShoppingCartEvent | ShoppingCartEvent[] => {
-  switch (type) {
-    case 'OpenShoppingCart': {
-      return {
-        type: 'ShoppingCartOpened',
-        data: {
-          shoppingCartId: command.shoppingCartId,
-          clientId: command.clientId,
-          openedAt: command.now,
-        },
-      };
-    }
-
-    case 'AddProductItemToShoppingCart': {
-      if (shoppingCart.status !== ShoppingCartStatus.Pending) {
-        throw ShoppingCartErrors.CART_IS_ALREADY_CLOSED;
-      }
-      return {
-        type: 'ProductItemAddedToShoppingCart',
-        data: {
-          shoppingCartId: command.shoppingCartId,
-          productItem: command.productItem,
-        },
-      };
-    }
-
-    case 'RemoveProductItemFromShoppingCart': {
-      if (shoppingCart.status !== ShoppingCartStatus.Pending) {
-        throw ShoppingCartErrors.CART_IS_ALREADY_CLOSED;
-      }
-
-      assertProductItemExists(shoppingCart.productItems, command.productItem);
-
-      return {
-        type: 'ProductItemRemovedFromShoppingCart',
-        data: {
-          shoppingCartId: command.shoppingCartId,
-          productItem: command.productItem,
-        },
-      };
-    }
-
-    case 'ConfirmShoppingCart': {
-      if (shoppingCart.status !== ShoppingCartStatus.Pending) {
-        throw ShoppingCartErrors.CART_IS_ALREADY_CLOSED;
-      }
-
-      if (shoppingCart.productItems.length === 0) {
-        throw ShoppingCartErrors.CART_IS_EMPTY;
-      }
-
-      return {
-        type: 'ShoppingCartConfirmed',
-        data: {
-          shoppingCartId: command.shoppingCartId,
-          confirmedAt: command.now,
-        },
-      };
-    }
-
-    case 'CancelShoppingCart': {
-      if (shoppingCart.status !== ShoppingCartStatus.Pending) {
-        throw ShoppingCartErrors.CART_IS_ALREADY_CLOSED;
-      }
-
-      return {
-        type: 'ShoppingCartCanceled',
-        data: {
-          shoppingCartId: command.shoppingCartId,
-          canceledAt: command.now,
-        },
-      };
-    }
-    default: {
-      const _: never = command;
-      throw ShoppingCartErrors.UNKNOWN_COMMAND_TYPE;
-    }
+): ProductItemAddedToShoppingCart => {
+  if (shoppingCart.status !== ShoppingCartStatus.Pending) {
+    throw ShoppingCartErrors.CART_IS_ALREADY_CLOSED;
   }
+  return {
+    type: 'ProductItemAddedToShoppingCart',
+    data: {
+      shoppingCartId: command.shoppingCartId,
+      productItem: command.productItem,
+    },
+  };
 };
 
-export const getAndUpdate = (
-  eventStore: EventStore,
-  streamId: string,
-  command: ShoppingCartCommand
-) => {
-  const events = eventStore.readStream<ShoppingCartEvent>(streamId);
+export const removeProductItemFromShoppingCart = (
+  command: RemoveProductItemFromShoppingCart,
+  shoppingCart: ShoppingCart
+): ProductItemRemovedFromShoppingCart => {
+  if (shoppingCart.status !== ShoppingCartStatus.Pending) {
+    throw ShoppingCartErrors.CART_IS_ALREADY_CLOSED;
+  }
 
-  const state = getShoppingCart(events);
+  assertProductItemExists(shoppingCart.productItems, command.productItem);
 
-  const result = decide(command, state);
-
-  eventStore.appendEvents(streamId, Array.isArray(result) ? result : [result]);
+  return {
+    type: 'ProductItemRemovedFromShoppingCart',
+    data: {
+      shoppingCartId: command.shoppingCartId,
+      productItem: command.productItem,
+    },
+  };
 };
+
+export const confirmShoppingCart = (
+  command: ConfirmShoppingCart,
+  shoppingCart: ShoppingCart
+): ShoppingCartConfirmed => {
+  if (shoppingCart.status !== ShoppingCartStatus.Pending) {
+    throw ShoppingCartErrors.CART_IS_ALREADY_CLOSED;
+  }
+
+  if (shoppingCart.productItems.length === 0) {
+    throw ShoppingCartErrors.CART_IS_EMPTY;
+  }
+
+  return {
+    type: 'ShoppingCartConfirmed',
+    data: {
+      shoppingCartId: command.shoppingCartId,
+      confirmedAt: command.now,
+    },
+  };
+};
+
+export const cancelShoppingCart = (
+  command: CancelShoppingCart,
+  shoppingCart: ShoppingCart
+): ShoppingCartCanceled => {
+  if (shoppingCart.status !== ShoppingCartStatus.Pending) {
+    throw ShoppingCartErrors.CART_IS_ALREADY_CLOSED;
+  }
+
+  return {
+    type: 'ShoppingCartCanceled',
+    data: {
+      shoppingCartId: command.shoppingCartId,
+      canceledAt: command.now,
+    },
+  };
+};
+
+export type Event<
+  EventType extends string = string,
+  EventData extends Record<string, unknown> = Record<string, unknown>
+> = Readonly<{
+  type: Readonly<EventType>;
+  data: Readonly<EventData>;
+}>;
+
+export const handleCommand =
+  <State, StreamEvent extends Event>(
+    evolve: (state: State, event: StreamEvent) => State,
+    getDefault: () => State
+  ) =>
+  (
+    eventStore: EventStore,
+    id: string,
+    handle: (state: State) => StreamEvent | StreamEvent[]
+  ) => {
+    const events = eventStore.readStream<StreamEvent>(id);
+
+    const state = events.reduce<State>(evolve, getDefault());
+
+    const result = handle(state);
+
+    eventStore.appendEvents(id, Array.isArray(result) ? result : [result]);
+  };
