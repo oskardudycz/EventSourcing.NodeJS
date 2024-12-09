2c5d39f5b5a526f8a1a9d62b6e07e6e08bbc5d90
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-09 11:12:44 +0100

    Added Emmett mongodb event store getting state code

--- workshops/introduction_to_event_sourcing/src/06_getting_state_from_events_emmett/mongodb/oop/gettingStateFromEvents.exercise.test.ts
+++ workshops/introduction_to_event_sourcing/src/06_getting_state_from_events_emmett/mongodb/oop/gettingStateFromEvents.exercise.test.ts
@@ -1,19 +1,13 @@
-import { getEventStoreDBTestClient } from '#core/testing/eventStoreDB';
 import {
-  ANY,
-  AppendResult,
-  EventStoreDBClient,
-  jsonEvent,
-} from '@eventstore/db-client';
+  getMongoDBTestClient,
+  releaseMongoDBContainer,
+} from '#core/testing/mongoDB';
+import { type Event } from '@event-driven-io/emmett';
+import {
+  getMongoDBEventStore,
+  MongoDBEventStore,
+} from '@event-driven-io/emmett-mongodb';
 import { v4 as uuid } from 'uuid';
-
-export type Event<
-  EventType extends string = string,
-  EventData extends Record<string, unknown> = Record<string, unknown>,
-> = Readonly<{
-  type: Readonly<EventType>;
-  data: Readonly<EventData>;
-}>;
 
 export interface ProductItem {
   productId: string;
@@ -78,30 +72,123 @@ enum ShoppingCartStatus {
   Canceled = 'Canceled',
 }
 
-export type ShoppingCart = Readonly<{
-  id: string;
-  clientId: string;
-  status: ShoppingCartStatus;
-  productItems: PricedProductItem[];
-  openedAt: Date;
-  confirmedAt?: Date;
-  canceledAt?: Date;
-}>;
+export class ShoppingCart {
+  constructor(
+    private _id: string,
+    private _clientId: string,
+    private _status: ShoppingCartStatus,
+    private _openedAt: Date,
+    private _productItems: PricedProductItem[] = [],
+    private _confirmedAt?: Date,
+    private _canceledAt?: Date,
+  ) {}
+
+  get id() {
+    return this._id;
+  }
+
+  get clientId() {
+    return this._clientId;
+  }
+
+  get status() {
+    return this._status;
+  }
+
+  get openedAt() {
+    return this._openedAt;
+  }
+
+  get productItems() {
+    return this._productItems;
+  }
+
+  get confirmedAt() {
+    return this._confirmedAt;
+  }
+
+  get canceledAt() {
+    return this._canceledAt;
+  }
+
+  public evolve = ({ type, data: event }: ShoppingCartEvent): void => {
+    switch (type) {
+      case 'ShoppingCartOpened': {
+        this._id = event.shoppingCartId;
+        this._clientId = event.clientId;
+        this._status = ShoppingCartStatus.Pending;
+        this._openedAt = event.openedAt;
+        this._productItems = [];
+        return;
+      }
+      case 'ProductItemAddedToShoppingCart': {
+        const {
+          productItem: { productId, quantity, unitPrice },
+        } = event;
+
+        const currentProductItem = this._productItems.find(
+          (pi) => pi.productId === productId && pi.unitPrice === unitPrice,
+        );
+
+        if (currentProductItem) {
+          currentProductItem.quantity += quantity;
+        } else {
+          this._productItems.push(event.productItem);
+        }
+        return;
+      }
+      case 'ProductItemRemovedFromShoppingCart': {
+        const {
+          productItem: { productId, quantity, unitPrice },
+        } = event;
+
+        const currentProductItem = this._productItems.find(
+          (pi) => pi.productId === productId && pi.unitPrice === unitPrice,
+        );
+
+        if (!currentProductItem) {
+          return;
+        }
+
+        currentProductItem.quantity -= quantity;
+
+        if (currentProductItem.quantity <= 0) {
+          this._productItems.splice(
+            this._productItems.indexOf(currentProductItem),
+            1,
+          );
+        }
+        return;
+      }
+      case 'ShoppingCartConfirmed': {
+        this._status = ShoppingCartStatus.Confirmed;
+        this._confirmedAt = event.confirmedAt;
+        return;
+      }
+      case 'ShoppingCartCanceled': {
+        this._status = ShoppingCartStatus.Canceled;
+        this._canceledAt = event.canceledAt;
+        return;
+      }
+    }
+  };
+}
 
 const appendToStream = async (
-  eventStore: EventStoreDBClient,
+  eventStore: MongoDBEventStore,
   streamName: string,
   events: ShoppingCartEvent[],
-): Promise<AppendResult> => {
-  const serializedEvents = events.map(jsonEvent);
+): Promise<bigint> => {
+  const { nextExpectedStreamVersion } = await eventStore.appendToStream(
+    streamName,
+    events,
+  );
 
-  return eventStore.appendToStream(streamName, serializedEvents, {
-    expectedRevision: ANY,
-  });
+  return nextExpectedStreamVersion;
 };
 
 export const getShoppingCart = (
-  _eventStore: EventStoreDBClient,
+  _eventStore: MongoDBEventStore,
   _streamName: string,
 ): Promise<ShoppingCart> => {
   // 1. Add logic here
@@ -109,10 +196,17 @@ export const getShoppingCart = (
 };
 
 describe('Events definition', () => {
-  let eventStore: EventStoreDBClient;
+  let eventStore: MongoDBEventStore;
 
   beforeAll(async () => {
-    eventStore = await getEventStoreDBTestClient();
+    const client = await getMongoDBTestClient();
+
+    eventStore = getMongoDBEventStore({ client });
+  });
+
+  afterAll(async () => {
+    await eventStore.close();
+    await releaseMongoDBContainer();
   });
 
   it('all event types should be defined', async () => {
@@ -187,7 +281,7 @@ describe('Events definition', () => {
       },
     ];
 
-    const streamName = `shopping_cart-${shoppingCartId}`;
+    const streamName = `shopping_cart:${shoppingCartId}`;
 
     await appendToStream(eventStore, streamName, events);
 
