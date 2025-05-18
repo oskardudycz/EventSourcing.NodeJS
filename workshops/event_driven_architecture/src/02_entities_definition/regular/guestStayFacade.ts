20e83c982d0d455aeec950933f4990cdd2eca6b1
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-05-18 11:47:19 +0200

    Added exercise with entity definition

---
+++ workshops/event_driven_architecture/src/02_entities_definition/regular/guestStayFacade.ts
@@ -0,0 +1,105 @@
+import type { Database, EventBus } from '../tools';
+import type { GuestStayAccountEvent } from './guestStayAccounts';
+
+export type CheckInGuest = {
+  type: 'CheckInGuest';
+  data: {
+    guestStayId: string;
+    guestId: string;
+    roomId: string;
+    now: Date;
+  };
+};
+export type RecordCharge = {
+  type: 'RecordCharge';
+  data: {
+    guestStayId: string;
+    amount: number;
+    now: Date;
+  };
+};
+
+export type RecordPayment = {
+  type: 'RecordPayment';
+  data: {
+    guestStayId: string;
+    amount: number;
+    now: Date;
+  };
+};
+
+export type CheckoutGuest = {
+  type: 'CheckoutGuest';
+  data: {
+    guestStayId: string;
+    now: Date;
+    groupCheckoutId?: string | undefined;
+  };
+};
+
+export type GuestStayAccountCommand =
+  | CheckInGuest
+  | RecordCharge
+  | RecordPayment
+  | CheckoutGuest;
+
+export type InitiateGroupCheckout = {
+  type: 'InitiateGroupCheckout';
+  data: {
+    groupCheckoutId?: string;
+    clerkId: string;
+    guestStayIds: string[];
+    now: Date;
+  };
+};
+
+export type GroupCheckoutCommand = InitiateGroupCheckout;
+
+export const GuestStayFacade = (options: {
+  database: Database;
+  eventBus: EventBus;
+}) => {
+  const { database, eventBus } = options;
+
+  const accounts = database.collection('guestStayAccount');
+
+  return {
+    checkInGuest: (_command: CheckInGuest) => {
+      throw new Error(
+        'TODO: Fill the implementation calling your entity/aggregate',
+      );
+    },
+    recordCharge: (command: RecordCharge) => {
+      const account = accounts.get(command.data.guestStayId);
+
+      if (!account) {
+        throw new Error('Entity not found');
+      }
+
+      // TODO: Fill the business logic implementation calling your entity/aggregate
+      // e.g. account.doSomething;
+      const events: GuestStayAccountEvent[] = []; // fill your events
+
+      accounts.store(command.data.guestStayId, account);
+      eventBus.publish(events);
+      throw new Error(
+        'TODO: Fill the implementation calling your entity/aggregate',
+      );
+    },
+    recordPayment: (_command: RecordPayment) => {
+      throw new Error(
+        'TODO: Fill the implementation calling your entity/aggregate',
+      );
+    },
+    checkOutGuest: (_command: CheckoutGuest) => {
+      throw new Error(
+        'TODO: Fill the implementation calling your entity/aggregate',
+      );
+    },
+    initiateGroupCheckout: (_command: InitiateGroupCheckout) => {
+      throw new Error(
+        'TODO: Fill the implementation calling your entity/aggregate',
+      );
+    },
+  };
+};
